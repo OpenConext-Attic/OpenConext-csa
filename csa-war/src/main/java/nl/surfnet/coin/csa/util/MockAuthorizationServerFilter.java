96c754057fb6f21821b0e6b3aee8271f06dd2d7a
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2013-06-06 15:59:43 +0200

    upped to new apis snapshot version

--- csa-war/src/main/java/nl/surfnet/coin/csa/util/MockAuthorizationServerFilter.java
+++ csa-war/src/main/java/nl/surfnet/coin/csa/util/MockAuthorizationServerFilter.java
@@ -41,7 +41,7 @@ public class MockAuthorizationServerFilter implements Filter {
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    AuthenticatedPrincipal principal = new SAMLAuthenticatedPrincipal("john.doe", Arrays.asList(new String[]{"user"}), new HashMap<String, String>(), Arrays.asList(new String[]{"csa_shopmanager"}), "http://mock-idp", "John Doe");
+    AuthenticatedPrincipal principal = new SAMLAuthenticatedPrincipal("john.doe", Arrays.asList(new String[]{"user"}), new HashMap<String, String>(), Arrays.asList(new String[]{"csa_shopmanager"}), "http://mock-idp", "John Doe", false);
     VerifyTokenResponse tokenResponse = new VerifyTokenResponse("client-name-mocked", Arrays.asList(new String[]{"read", AuthorityScopeInterceptor.OAUTH_CLIENT_SCOPE_ACTIONS}), principal, null);
     request.setAttribute(AuthorizationServerFilter.VERIFY_TOKEN_RESPONSE, tokenResponse);
     chain.doFilter(request, response);
